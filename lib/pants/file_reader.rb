fc265f6f296abc43243f6a2006c97c98ee41e94a
Steve Loveless <steve.loveless@gmail.com>
2012-12-20 10:39:48 -0800

    Added docs, cleaned up logging

--- lib/pants/file_reader.rb
+++ lib/pants/file_reader.rb
@@ -3,31 +3,53 @@ require_relative 'logger'
 
 
 class Pants
-  class FileReadConnection < EventMachine::Connection
+
+  # This is the EventMachine connection that reads the source file and puts
+  # the read data into the data channel so writers can write as they need to.
+  class FileReaderConnection < EventMachine::Connection
     include LogSwitch::Mixin
 
+    # @param [EventMachine::Channel] data_channel The data channel to write read
+    #   data to.
+    #
+    # @param [EventMachine::Deferrable] finisher Gets set to succeeded when the
+    #   file-to-read has been fully read.
     def initialize(data_channel, finisher)
       @data_channel = data_channel
       @finisher = finisher
     end
 
+    # Reads the data and writes it to the data channel.
+    #
+    # @param [String] data The file data to write to the channel.
     def receive_data(data)
       @data_channel << data
     end
 
+    # Called when the file is done being read.
     def unbind
       log "Unbinding"
       @finisher.succeed
     end
   end
 
+
+  # This is the interface for FileReaderConnections.  It controls starting,
+  # stopping, and threading the connection.
   class FileReader
     include LogSwitch::Mixin
 
+    # The block to be called when starting up.  Writers should all have been
+    # added before calling this; if writers are started after this, they won't
+    # get the first bytes that are read (due to start-up time).
     attr_reader :starter
 
+    # @param [EventMachine::Channel] data_channel The channel to write to, so
+    #   that all writers can do their thing.
+    #
+    # @param [String] file_path Path to the file to read.
     def initialize(data_channel, file_path)
-      file = File.open(file_path, 'r')
+      file = File.open(file_path, 'rb')
 
       finisher = EM::DefaultDeferrable.new
 
@@ -46,7 +68,7 @@ class Pants
       @starter = proc do |writers|
         @writers = writers
         log "Opening and adding file at #{file_path}..."
-        EM.attach(file, FileReadConnection, data_channel, finisher)
+        EM.attach(file, FileReaderConnection, data_channel, finisher)
       end
 
       if EM.reactor_running?
--- lib/pants/file_writer.rb
+++ lib/pants/file_writer.rb
@@ -3,13 +3,19 @@ require_relative 'logger'
 
 
 class Pants
+
+  # This is the "connection" (EventMachine term) that connects the data from
+  # the data channel to the file to write to.
   class FileWriterConnection < EM::Connection
     include LogSwitch::Mixin
 
+    # @param [EventMachine::Channel] data_channel The channel to expect data on
+    #   and write to the file (self).
     def initialize(data_channel)
       @data_channel = data_channel
     end
 
+    # Waits for data on the channel, then writes it out to file.
     def post_init
       @data_channel.subscribe do |data|
         begin
@@ -22,23 +28,35 @@ class Pants
       end
     end
 
+    # Makes sure the file is flushed before being closed.
     def unbind
+      log "#{__id__} is unbinding"
       close_connection_after_writing
-      log "Unbinding"
       @io.flush
     end
   end
 
+
+  # This is the interface for FileWriterConnections.  It controls starting,
+  # stopping, and threading the connection.
   class FileWriter
     include LogSwitch::Mixin
+
+    # The block to be called when starting up a new Pants reader.
     attr_reader :starter
+
+    # The block to be called when the reader is done reading.
     attr_reader :finisher
 
+    # @param [EventMachine::Channel] data_channel The channel to read data from
+    #   and thus write to file.
+    #
+    # @param [String] file_path The path to write to.
     def initialize(data_channel, file_path)
       file = file_path.is_a?(File) ? file_path : File.open(file_path, 'w')
 
       @starter = proc do
-        log "Adding a #{self.class} at #{file_path}..."
+        log "#{__id__} Adding a #{self.class} to write to #{file_path}"
 
         EM.defer do
           EM.attach(file, FileWriterConnection, data_channel)
@@ -46,12 +64,11 @@ class Pants
       end
 
       @finisher = proc do
-        log "Finisher"
+        log "Finishing ID #{__id__}"
         file.close unless file.closed?
       end
 
       if EM.reactor_running?
-        log "Reactor is runnning..."
         @starter.call
       end
     end
