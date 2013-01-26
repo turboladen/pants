a6fcc6d5d6367b3f722c8d5d39f9672b66929fa0
Steve Loveless <steve.loveless@gmail.com>
2013-01-25 17:58:01 -0800

    Test codez

--- test_readers.rb
+++ test_readers.rb
@@ -11,11 +11,11 @@ orig_file = 'spec/support/pants.wav'
 dest_file = 'dest_test_file'
 #Pants.read('udp://127.0.0.1:1234') do |reader|
 Pants.read(orig_file) do |reader|
-  1.times do |i|
+  10.times do |i|
     reader.add_writer(dest_file)
     reader.add_writer("udp://127.0.0.1:#{1235 + i}")
   end
-
+  reader.add_writer('udp://10.221.222.90:9000')
 end
 
 if defined? orig_file
@@ -25,3 +25,19 @@ if defined? orig_file
   puts "Original file size: #{orig_file_size}"
   puts "Difference: #{orig_file_size - pants_file_size}"
 end
+
+#==============
+core = Pants::Core.new
+udp_reader = core.read 'udp://127.0.0.1:1234'
+udp_reader.write_to 'udp://127.0.0.1:1235'
+
+udp_reader2 = core.add_reader(Pants::Readers::UDPReader, '127.0.0.1', 1234)
+udp_reader2.add_writer(Pants::Writers::UDPWriter, '127.0.0.1', 1235)
+
+udp_reader3 = Pants::Readers::UDPReader.new('127.0.0.1', 1234, core.callback)
+udp_writer3 = Pants::Writers::UDPWriter.new('127.0.0.1', 1235, udp_reader3.write_to_channel)
+core.add_reader(udp_reader3)
+udp_reader3.add_writer(udp_writer3)
+
+seam = udp_reader.add_seam(Pants::SimpleSeam)
+seam.write_to 'my_file'
--- test_seams.rb
+++ test_seams.rb
@@ -1,5 +1,20 @@
-
+=begin
 require './lib/pants'
+
+
+class Pants::SimpleSeam < Pants::Seam
+  def start
+    callback = EM.Callback do
+      log "Starting simple seam..."
+      read do |data|
+        write data
+      end
+    end
+
+    super(callback)
+  end
+end
+
 
 orig_file = 'dest_test_file'
 seamed_file = 'dest_seamed_file'
@@ -16,3 +31,77 @@ if defined? orig_file
   puts "Original file size: #{orig_file_size}"
   puts "Difference: #{orig_file_size - seamed_file_size}"
 end
+
+#-------------------------------------------------------------------------------
+Pants.mix do |mixer|
+  mixer.add_reader('udp://127.0.0.1:1234')
+  mixer.add_reader('udp://127.0.0.1:1235')
+  mixer.add_writer(seamed_file)
+end
+
+#-------------------------------------------------------------------------------
+core = Pants::Core.new
+gatherer = Pants::Gatherer.new
+
+gatherer.add_reader('udp://127.0.0.1:1234')
+gatherer.add_reader('udp://127.0.0.1:1235')
+
+core.run
+#-------------------------------------------------------------------------------
+=end
+require './lib/pants'
+
+class PantsInspector < Pants::Seam
+  def initialize(core_callback, reader_channel, host)
+    @host = host
+
+    super(core_callback, reader_channel)
+  end
+
+  # Pants will call this for you when it starts the reader that the seam is
+  # reading from.
+  def start
+
+    # You need to define a callback here so pants can call you back after its
+    # made sure that the seam's writers have been started.  This makes sure
+    # the seam doesn't start reading and pushing out data before the writers
+    # are ready to receive it.
+    callback = EM.Callback do
+      puts "Starting #{self.class}..."
+
+      read_items do |data|
+        if data.match(/pants/mi)
+          data << "Pants party at #{@host}!!!!!!\n"
+        end
+
+        write data
+      end
+    end
+
+    super(callback)
+  end
+end
+
+# Assuming you have data coming in on this IP and UDP port, this reads each
+# packet and hands it over to its writers--in this case, the file dump file
+# writer, the UDP writer that sends to 10.0.0.50:9001, and our PacketInspector
+# seam.
+Pants.read(__FILE__) do |reader|
+
+  # Dump UDP packets to a file
+  reader.write_to 'file_dump_of_udp_packets.udp'
+
+  # Redirect the UDP packets to another host
+  reader.write_to 'udp://127.0.0.1:9001'
+
+  # Inspect the UDP packets and notify about the party going on
+  pants_inspector = reader.add_seam(PantsInspector, 'localhost')
+
+  # The packet inspector seam behaves like a reader, which lets writers read
+  # from it.  Dump those party packets to a file
+  pants_inspector.write_to 'file_dump_of_party_packets.party'
+
+  # Forward our partying packets on to another host.
+  pants_inspector.write_to 'udp://127.0.0.1:9002'
+end
+
